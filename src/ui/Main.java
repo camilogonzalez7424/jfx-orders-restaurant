af007ac5397984a618b3d24418874956528b4121
camilogonzalez7424 <gonzalezcamilo508@gmail.com>
2021-03-07 21:07:25 -0500

    Main Panel implemented

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,32 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application{
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Main.fxml"));
+
+        //Colocar controladora
+
+        //fxmlLoader.setController(contactManagerGUI);
+
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("LA CASA DORADA");
+        primaryStage.show();
     }
 }
